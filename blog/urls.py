f06029098f9c97fd8f2579aa3d726cc0278e4800
jaegray <jae_gray@yahoo.com>
2022-01-01 12:37:34 -0500

    Revert "Files"
    This reverts commit 8bdb0aaf528b862a07c9bd7d890e69180a3c4d03.

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views  # from current folder import views
-
-urlpatterns = [
-    path("", views.starting_page, name="starting-page"),  # creating a name to make things easier
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug>", views.post_detail, name="posts-detail-page")  # this is dynamic. /posts/my-first-post is
-    # referred to as a slug
-]
