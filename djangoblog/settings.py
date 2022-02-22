226c9a674578d329460a955a5a6a3f8f690182cf
Jae <82623058+Jaegray@users.noreply.github.com>
2022-02-22 00:13:25 -0500

    Update settings.py

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -110,7 +110,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS files, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'  # static files
